f5bad8d11e37917acfe77ceb4dcc618abc232de7
freetimezp <kraftukrnet@ukr.net>
2025-02-21 17:10:29 +0200

    02. usefull-things-029-parallax. UI. SimpleParallax animation.

---
+++ assets/js/main.js
@@ -0,0 +1,40 @@
+var image = document.getElementsByClassName("ione");
+new simpleParallax(image, {
+    scale: 1.8
+});
+
+
+var image2 = document.getElementsByClassName("itwo");
+new simpleParallax(image2, {
+    scale: 1.6,
+    orientation: 'left'
+});
+
+var image3 = document.getElementsByClassName("ithree");
+new simpleParallax(image3, {
+    scale: 1.6,
+    orientation: 'right'
+});
+
+var image4 = document.getElementsByClassName("ifour");
+new simpleParallax(image4, {
+    scale: 1.6
+});
+
+var image5 = document.getElementsByClassName("ifive");
+new simpleParallax(image5, {
+    scale: 1.6,
+    orientation: 'right'
+});
+
+var image6 = document.getElementsByClassName("isix");
+new simpleParallax(image6, {
+    scale: 1.6,
+    orientation: 'right'
+});
+
+
+
+
+
+
